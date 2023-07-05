484d401b5277b15a725dde2cac76809cb00eac75
John Duprey <jwd@johnwduprey.com>
2023-07-04 21:36:18 -0400

    Update run.ps1

--- BestPracticeAnalyser_GetQueue/run.ps1
+++ BestPracticeAnalyser_GetQueue/run.ps1
@@ -1,3 +1,4 @@
+param($name)
 Set-Location (Get-Item $PSScriptRoot).Parent.FullName
 #$Skiplist = (Get-Content ExcludedTenants -ErrorAction SilentlyContinue | ConvertFrom-Csv -Delimiter "|" -Header "name", "date", "user").name
 $Tenants = Get-Tenants #Get-Content ".\tenants.cache.json" | ConvertFrom-Json | Where-Object {$Skiplist -notcontains $_.defaultDomainName}
@@ -6,5 +7,3 @@ $object = foreach ($Tenant in $Tenants) {
     $Tenant.defaultDomainName
 }
 Write-LogMessage -API 'BestPracticeAnalyser' -tenant 'None' -message "running BPA for $($tenants.count) tenants" -sev info
-
-$object
